88b1d3f62f34cb341f8e8c3a9c275f51e59f798e
Maksim Terentiev <maksim_terentiev@mail.ru>
2022-02-23 17:20:36 +0300

    Heap sort

--- prog.pas
+++ prog.pas
@@ -36,7 +36,7 @@ procedure generate_array(var arr:T_arr; n:integer; gen_type:integer);
   const MaxAdd=10;
   var i,j:integer;
   begin
-      { writeln('gen sorted'); }
+      { writeln('gen inverted'); }
       j:=random(MaxAdd);
       for i:=0 to n-1 do
       begin
@@ -48,7 +48,7 @@ procedure generate_array(var arr:T_arr; n:integer; gen_type:integer);
   const MaxSub=10;
   var i,j:integer;
   begin
-      { writeln('gen inverted'); }
+      { writeln('gen sorted'); }
       j:=random(MaxSub);
       for i:=1 to n do
       begin
@@ -72,22 +72,22 @@ begin
 	end;
 end;
 
+procedure swap(var a,b:T_elem);
+var tmp:T_elem;
+begin
+	tmp:=a;
+	a:=b;
+	b:=tmp;
+end;
 
 procedure bubble_sort(var arr:T_arr; n:integer);
 var sorted:boolean;
     i:integer;
     cmp_count,swap_count:integer;
-  procedure swap(var a,b:T_elem);
-  var tmp:T_elem;
-  begin
-      tmp:=a;
-      a:=b;
-      b:=tmp;
-  end;
 begin
 	cmp_count:=0; swap_count:=0;
 
-	writeln('* Bubble sort started *');
+	writeln('** Bubble sort started *');
 	{$ifdef VERBOSE}
 	write('* '); write_array(arr,n);
 	{$endif}
@@ -110,7 +110,7 @@ begin
 		write('* '); write_array(arr,n);
 		{$endif}
 	end;
-	writeln('* Bubble sort ended   *');
+	writeln('** Bubble sort ended   *');
 	writeln('* cmp=',cmp_count);
 	writeln('* swap=',swap_count);
 	writeln
@@ -118,26 +118,95 @@ end;
 
 
 procedure heap_sort(var arr:T_arr; n:integer);
+var swap_count,cmp_count:integer;
+  function left(i:integer):integer;
+    begin left:= 2*i+1 end;
+  function right(i:integer):integer;
+    begin right:= 2*i+2 end;
+  function parent(i:integer):integer;
+    begin parent:= (i-1) div 2 end;
+  procedure min_heapify(var arr:T_arr;n,i:integer);
+    var l,r,smallest:integer;
+    begin
+        l:=left(i);
+        r:=right(i);
+        inc(cmp_count);inc(cmp_count);
+        if (l<n) and (arr[l] < arr[i])
+            then smallest:= l
+            else smallest:=i;
+        inc(cmp_count);inc(cmp_count);
+        if (r<n) and (arr[r] < arr[smallest])
+            then smallest:=r;
+        inc(cmp_count);
+        if smallest<>i then
+        begin
+            inc(swap_count);
+            swap(arr[i],arr[smallest]);
+            min_heapify(arr,n,smallest);
+        end;
+    end;
+  procedure build_min_heap(var arr:T_arr;n:integer);
+  var i:integer;
+  begin
+      for i:= (n-1) div 2 downto 0 do       { ? }
+          min_heapify(arr,n,i);
+  end;
 begin
+	cmp_count:=0; swap_count:=0;
+	writeln('** Heap sort started *');
+	{$ifdef VERBOSE}
+	write('* Init:      '); write_array(arr,n);
+	{$endif}
 
+	build_min_heap(arr,n);
+	{$ifdef VERBOSE}
+	write('* Min_heap:  '); write_array(arr,n);
+	{$endif}
+	for i:=(n-1) downto 1 do
+	begin
+		inc(swap_count);
+		swap(arr[0],arr[i]);
+		{$ifdef VERBOSE_HEAP_SWAP}
+		write('* Swap:      '); write_array(arr,n);
+		{$endif}
+		min_heapify(arr,i,0);
+		{$ifdef VERBOSE}
+		write('* Sort step: '); write_array(arr,n);
+		{$endif}
+	end;
+	writeln('** Heap sort ended *');
+	writeln('* cmp=',cmp_count);
+	writeln('* swap=',swap_count);
+	writeln
 end;
 
 
 procedure testing; { temporaly for testing generation and bubble }
-var arr:T_arr;
+var arr,init_arr:T_arr;
     n:integer;
     i:integer;
 begin
 	n:=6;                            
 	for i:=1 to 4 do                 
 	begin                            
-		generate_array(arr,n,i);  
+		generate_array(init_arr,n,i);  
 		write(i,': ');            
-		write_array(arr, n);      
+		write_array(init_arr, n);      
+
+		arr:=copy(init_arr,0,length(init_arr)); {dynArray copy}
 		bubble_sort(arr,n);
 		write('arr=');
 		write_array(arr,n);
-		writeln
+		writeln;
+
+
+		arr:=copy(init_arr,0,length(init_arr)); {dynArray copy}
+		write(i,': ');            
+		write_array(arr, n);      
+		heap_sort(arr,n);
+		write('arr=');
+		write_array(arr,n);
+		writeln;
 	end;                             
 end;
 
